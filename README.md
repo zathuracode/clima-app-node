f10f7560d9d45c4224ef584161e31d305a740dc4
Diego Armando Gomez Mosquera <diegomezusb@gmail.com>
2020-08-19 12:14:47 -0500

    Carga inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicacion del clima
+
+Este es un demo de aplicacion del clima en modo texto
+
+Para instalar los paquetes
+
+```
+npm install
+```
---
+++ app.js
@@ -0,0 +1,30 @@
+const lugar = require('./lugar');
+const clima = require('./clima');
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+
+const getInfo = async( direccion ) => {
+
+
+    try {
+        const coords = await lugar.getLugarLatLng( direccion );
+        const temp   = await clima.getClima( coords.lat, coords.lng );
+        return `El clima de ${ coords.direccion } es de ${ temp }.`;
+    } catch (e) {
+        return `No se pudo determinar el clima de ${ direccion }`;
+    }
+
+}
+
+
+
+getInfo( argv.direccion )
+    .then( console.log )
+    .catch( console.log );
